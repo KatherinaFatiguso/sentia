54d4a0f30e2afe9d73fda2dfa68394f6baa63322
Katherina Fatiguso <katherina.fatiguso@aarnet.edu.au>
2019-11-03 23:40:05 +1100

    Setup CSV parser for affiliations table.

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -1,3 +1,17 @@
 class Affiliation < ApplicationRecord
+  require 'csv'
   belongs_to :person
+  def self.import
+    table = CSV.parse(File.read('data/csv_sheet1.csv'), headers: true)
+    # puts table.by_col[4]
+    # puts table.by_col[4].count
+    loc = table.by_col[4]
+    loc.each do |row|
+      if !row.nil?
+        self.find_or_create_by!(
+          name: row
+        )
+      end
+    end
+  end
 end
