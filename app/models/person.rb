94fd7f135fed17de70956c256debe5bb22daa6ba
Katherina Fatiguso <katherina.fatiguso@aarnet.edu.au>
2019-11-04 04:00:01 +1100

    Add CSV parse in people table

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,11 @@
 class Person < ApplicationRecord
   require 'csv'
-  # validates :first_name :locations :species :gender :affiliations presence: true
+  validates :first_name, :locations, :species, :gender, :affiliations, presence: true
 
   # has_many :locations
-  # has_many :affiliations
+  has_many :affiliations
+  has_many :locations, :foreign_key => "id", :class_name => "Location"
+
 
   def self.import
     table = CSV.parse(File.read('data/csv_sheet1.csv'), headers: true)
@@ -14,24 +16,42 @@ class Person < ApplicationRecord
     #   puts row.by_row[]
     #
     # end
-    for i in 0..table.count-1 do
-      # puts i
-      # puts table.by_row[i]["Name"].split(",", 2)
-      name = table.by_row[i]["Name"]
-      split_name = name.split(" ", 2)
-      # puts split_name[0].titlecase # First name
-      # if !split_name[1].nil?
-      #   puts split_name[1].titlecase # Second name
-      # end
-      # puts table.by_row[i]["Location"].titlecase
-      # puts table.by_row[i]["Species"]
-      # puts table.by_row[i]["Gender"]
-      # puts table.by_row[i]["Affiliations"]
-      # if table.by_row[i]["Affiliations"].nil?
-      #   puts 'buzzzzzz'
-      # end
-      puts table.by_row[i]["Weapon"]
-      # puts table.by_row[i]["Vehicle"]
+    size = table.count-1
+    for i in 0..size do
+      puts i
+      # puts "OOOOOOOOOOO"
+      if !table.by_row[i]["Affiliations"].nil? # Only add Person with Affiliations
+        location = Location.find_by(name: table.by_row[i]["Location"].titlecase)
+        affiliation = Affiliation.find_by(name: table.by_row[i]["Affiliations"])
+        name = table.by_row[i]["Name"]
+        split_name = name.split(" ", 2)
+        # puts split_name[0].titlecase # First name
+        Person.find_or_create_by!(
+          first_name: split_name[0].titlecase,
+
+          # puts split_name[1].titlecase # Second name
+          last_name: !split_name[1].nil? ? split_name[1].titlecase : "",
+
+          # puts table.by_row[i]["Location"].titlecase
+          locations: location.id,
+
+          # puts table.by_row[i]["Species"]
+          species: table.by_row[i]["Species"],
+
+          # puts table.by_row[i]["Gender"]
+          gender: table.by_row[i]["Gender"],
+
+          # puts table.by_row[i]["Affiliations"]
+          affiliations: affiliation.id,
+
+          # puts table.by_row[i]["Weapon"]
+          weapon: !table.by_row[i]["Weapon"].nil? ? table.by_row[i]["Weapon"] : "",
+
+          # puts table.by_row[i]["Vehicle"]
+          vehicle: !table.by_row[i]["Vehicle"].nil? ? table.by_row[i]["Vehicle"] : "",
+        )
+
+      end
     end
   end
 
